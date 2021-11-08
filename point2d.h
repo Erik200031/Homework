d5169728795723e493691510091aae7b3adfb7ed
Erik200031 <90761494+Erik200031@users.noreply.github.com>
2021-11-08 16:36:45 +0400

    Update point2d.h

--- point2d.h
+++ point2d.h
@@ -6,25 +6,19 @@
 class Point2D{
 public:
   void set_x_y(double,double);
-  double get_x(); 
-  double get_y(); 
+  double get_x()const; 
+  double get_y()const; 
 public:
   Point2D(); 
   Point2D(double ,double ); 
   Point2D(const Point2D & ); 
   Point2D& operator=(const Point2D & ); 
   Point2D operator+(const Point2D & ); 
-  friend Point2D operator+(double , Point2D ); 
-  Point2D operator+=(const Point2D & ); 
+  Point2D& operator+=(const Point2D & ); 
   Point2D operator-(const Point2D & );
-  friend Point2D operator-(double , Point2D );
-  Point2D operator-=(const Point2D & );
+  Point2D& operator-=(const Point2D & );
   Point2D operator*(const Point2D & );
-  friend Point2D operator*(double , Point2D);
-  Point2D operator*=(const Point2D & );
-  Point2D operator/(const Point2D & );
-  friend Point2D operator/(double , Point2D);
-  Point2D operator/=(const Point2D & );
+  Point2D& operator*=(const Point2D & );
   bool operator<(const Point2D &)const;
   bool operator>(const Point2D &)const;
   bool operator==(const Point2D &)const;
